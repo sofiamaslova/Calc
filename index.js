d25b0f72ca54f362bfc2427d835daa884ea9be4f
Sofia Maslova <sonaya@mail.ru>
2018-09-06 15:24:37 +0700

    1 attempt

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-var button1 = document.getElementById("button-1");//document пространство нашей страницы getElementById - получение элемента из страницы
-var button2 = document.getElementById("button-2");
-var button3 = document.getElementById("button-3");
-var heading = document.getElementById("heading");
+//var button1 = document.getElementById("button-1");//document пространство нашей страницы getElementById - получение элемента из страницы
+//var button2 = document.getElementById("button-2");
+//var button3 = document.getElementById("button-3");
+//var button4 = document.getElementById("button-4");
 
-changeColor = function (color) {
-    return function (event) {
-        heading.style.color = color;
+var Fcalc = document.calc;
+var Currents = 0;
+var FlagNewNum = false;
+var PendingOp = "";
+
+
+function NumPressed (Num)
+{
+    if (FlagNewNum)
+    {
+        Fcalc.ReadOut.value = Num;
+        FlagNewNum = false;
     }
+    else
+    {
+        if (Fcalc.ReadOut.value === "0")
+            Fcalc.ReadOut.value = Num;
+        else
+            Fcalc.ReadOut.value += Num;
+    }
+}
+
+function Operation (Op)
+{
+    var Readout = Fcalc.ReadOut.value;
+    if (FlagNewNum && PendingOp != "=")
+    {
+        Fcalc.ReadOut.value = Currents;
+    }
+    else
+    {
+        FlagNewNum = true;
+        if ( '+' == PendingOp )
+            Currents += parseFloat(Readout);
+        else if ( '-' == PendingOp )
+            Currents -= parseFloat(Readout);
+        else if ( '/' == PendingOp )
+            Currents /= parseFloat(Readout);
+        else if ( '*' == PendingOp )
+            Currents *= parseFloat(Readout);
+        else
+            Currents = parseFloat(Readout);
+        Fcalc.ReadOut.value = Currents;
+        PendingOp = Op;
+    }
+}
+
+
+
+
+
+
+heading = document.getElementById("heading");
+
+//changeColor = function (color) {
+//    return function (event) {
+//        heading.style.color = color;
+//    }
 };
-button1.onclick = changeColor("red");
-button2.onclick = changeColor("green");
-button3.onclick = changeColor("blue");
+// button1.onclick = changeColor("red");
+// button2.onclick = changeColor("green");
+//button3.onclick = changeColor("blue");
 increment = function (event) {
     var i = parseInt(event.target.innerHTML);
     event.target.innerHTML = i + 1;
@@ -23,6 +77,8 @@ for (var i = 0; i < buttons.length; i++) {
     button.addEventListener("click", increment)
 }
 
-button1.addEventListener("click", changeColor("red"));
+button1.addEventListener("click", );
 button2.addEventListener("click", changeColor("green"));
 button3.addEventListener("click", changeColor("blue"));
+button4.addEventListener("click", changeColor("darkgrey"));
+console.log(button);
